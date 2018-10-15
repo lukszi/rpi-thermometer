97c72b8a04e55dc394db76ee26f2a251d488ec7f
lukas <lukas.szimtenings@rwth-aachen.de>
2018-10-15 18:38:03 +0200

    changed the library again

--- information_provider/BME280InformationProvider.py
+++ information_provider/BME280InformationProvider.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import board
 import busio
-from adafruit_bmp280 import Adafruit_BMP280_I2C
+from adafruit_bmp280 import Adafruit_BME280_I2C
 
 from information_provider.BaseInformationProvider import BaseInformationProvider
 from database import DB
@@ -11,7 +11,7 @@ class BME280InformationProvider(BaseInformationProvider):
     def __init__(self, database: DB):
         super(BME280InformationProvider, self).__init__(database)
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.bme280 = Adafruit_BMP280_I2C(self.i2c, address=0x76)
+        self.bme280 = Adafruit_BME280_I2C(self.i2c, address=0x76)
 
     def get_current_data(self, item):
         return {
