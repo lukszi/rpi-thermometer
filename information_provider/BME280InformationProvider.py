c7e255bc8cb8cc30e33e67a51e123da7c1e322c8
lukas <lukas.szimtenings@rwth-aachen.de>
2018-10-15 09:24:28 +0200

    removed unused import trying to use bme-lib to run bmp

--- information_provider/BME280InformationProvider.py
+++ information_provider/BME280InformationProvider.py
@@ -11,6 +11,7 @@ class BME280InformationProvider(BaseInformationProvider):
     def __init__(self, database: DB):
         super(BME280InformationProvider, self).__init__(database)
         self.i2c = busio.I2C(board.SCL, board.SDA)
+        adafruit_bme280._BME280_ADDRESS = 76
         self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(self.i2c)
 
     def get_current_data(self, item):
--- information_provider/BaseInformationProvider.py
+++ information_provider/BaseInformationProvider.py
@@ -1,5 +1,4 @@
 from abc import ABCMeta, abstractmethod
-from datetime import datetime
 from database import DB
 
 
