0790b1e967198b117391ec3c7c917b4f822e7db2
ls440063 <lukas.szimtenings@rwth-aachen.de>
2018-10-17 22:55:56 +0200

    Changed databaselogger to use rest service

--- data_logger/run.py
+++ data_logger/run.py
@@ -3,6 +3,7 @@ import json
 from data_logger.UpdateDaemon import UpdateDaemon
 from database import DB
 from information_provider.DummyInformationProvider import DummyInformationProvider
+from information_provider.RESTInformationProvider import RESTInformationProvider
 from util import Config
 
 if __name__ == '__main__':
@@ -10,6 +11,6 @@ if __name__ == '__main__':
     database_conf = conf["database"]
     daemon_conf = conf["daemon"]
     db = DB(database_conf["server"], database_conf["user"], database_conf["password"])
-    daemon = UpdateDaemon(db, DummyInformationProvider(db), daemon_conf["polling_interval"],
+    daemon = UpdateDaemon(db, RESTInformationProvider("http://msu.oph.rwth-aachen.de/"), daemon_conf["polling_interval"],
                           json.loads(daemon_conf["item_list"]))
     daemon.run()
